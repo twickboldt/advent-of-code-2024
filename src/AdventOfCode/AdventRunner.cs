474d42f1f1e06799aa94855b3dbfb908db99dbb5
Timo Wickboldt <tw@comparit.de>
2024-12-06 07:45:02 +0100

    feat: run all implmentations with execution time

--- src/AdventOfCode/AdventRunner.cs
+++ src/AdventOfCode/AdventRunner.cs
@@ -14,15 +14,24 @@ public class AdventRunner
 
     public async Task Run()
     {
-        var lastImplementation = _implementations.OrderByDescending(i => i.GetType().Name).First();
-        var dayNumber = int.Parse(lastImplementation.GetType().Name.Replace("Day", ""));
+        var implementations = _implementations.OrderByDescending(i => i.GetType().Name);
+        foreach (var implementation in implementations)
+        {
+            var dayNumber = int.Parse(implementation.GetType().Name.Replace("Day", ""));
+            await ExecuteSolver(implementation, dayNumber);
+            Console.WriteLine();
+        }
+    }
+
+    private static async Task ExecuteSolver(Day lastImplementation, int dayNumber)
+    {
         try
         {
             var sw = new Stopwatch();
             sw.Start();
             var resultPuzzle1 = lastImplementation.SolvePuzzle1(await GetInput(dayNumber));
             sw.Stop();
-            Console.WriteLine($"Day {dayNumber} Puzzle 1: {resultPuzzle1} took: {sw.Elapsed}");
+            Console.WriteLine($"Day {dayNumber} Puzzle 1: {resultPuzzle1} took: {sw.Elapsed} | {sw.ElapsedMilliseconds}ms");
 
             var puzzle2HasDifferentInput = File.Exists($"inputs/day{dayNumber}_{2}.txt");
             sw.Reset();
@@ -30,7 +39,7 @@ public class AdventRunner
             var resultPuzzle2 =
                 lastImplementation.SolvePuzzle2(await GetInput(dayNumber, puzzle2HasDifferentInput ? 2 : 1));
             sw.Stop();
-            Console.WriteLine($"Day {dayNumber} Puzzle 2: {resultPuzzle2} took: {sw.Elapsed}");
+            Console.WriteLine($"Day {dayNumber} Puzzle 2: {resultPuzzle2} took: {sw.Elapsed} | {sw.ElapsedMilliseconds}ms");
         }
         catch (Exception)
         {
